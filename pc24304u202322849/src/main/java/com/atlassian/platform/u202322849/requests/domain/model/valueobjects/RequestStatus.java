3af58f0beb7d8834c1444da9bf44f13830cf9420
V8Z5 <marlonvizaquispe@gmail.com>
2026-07-11 09:20:52 -0500

    Add request status value object

---
+++ pc24304u202322849/src/main/java/com/atlassian/platform/u202322849/requests/domain/model/valueobjects/RequestStatus.java
@@ -0,0 +1,8 @@
+package com.atlassian.platform.u202322849.requests.domain.model.valueobjects;
+
+/** Lifecycle status of an access request.
+ * @author Marlon Packard Viza Quispe
+ */
+public enum RequestStatus {
+    PENDING, APPROVED, REJECTED
+}
